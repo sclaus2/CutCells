e0d78149e5c47de71f9b7126a77dfb21bf25b5a6
Susanne Claus <Susanne.Claus@ucl.ac.uk>
2023-01-21 02:07:30 +0100

    pyvista visualization of cuts

--- python/demo/cut_tetrahedron/cut_tetrahedron.py
+++ python/demo/cut_tetrahedron/cut_tetrahedron.py
@@ -1,5 +1,6 @@
 import cutcells
 import numpy as np
+import pyvista as pv 
 
 ls_values = np.array([0.1,-0.1,0.2, 0.4])
 vertex_coordinates = np.array([1.,1.,1., 1.,-1., -1., -1, 1., -1., -1., -1, 1])
@@ -12,14 +13,26 @@ cell_type = cutcells.CellType.tetrahedron
 triangulate = False
 gdim = 3
 
-cut_cell = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi<0", triangulate)
-print(cut_cell.str())
-cut_cell.write_vtk("interior.vtu")
+cut_cell_int = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi<0", triangulate)
+print(cut_cell_int.str())
+cut_cell_int.write_vtk("interior.vtu")
 
-cut_cell = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi>0", triangulate)
-print(cut_cell.str())
-cut_cell.write_vtk("exterior.vtu")
+cut_cell_ext = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi>0", triangulate)
+print(cut_cell_ext.str())
+cut_cell_ext.write_vtk("exterior.vtu")
 
 cut_cell = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi=0", triangulate)
 print(cut_cell.str())
 cut_cell.write_vtk("interface.vtu")
+
+pv.start_xvfb()
+
+#grid = pv.UnstructuredGrid(cells, celltypes, points)
+
+grid_int = pv.UnstructuredGrid(cut_cell_int.connectivity, cut_cell_int.types, cut_cell_int.vertex_coords)
+grid_ext = pv.UnstructuredGrid(cut_cell_ext.connectivity, cut_cell_ext.types, cut_cell_ext.vertex_coords)
+
+plotter = pv.Plotter(off_screen=True)
+plotter.add_mesh(grid_int, color="blue",show_edges=True)
+plotter.add_mesh(grid_ext, color="red",show_edges=True, opacity=0.5)
+plotter.show(screenshot='cut_triangle.png')
--- python/demo/cut_triangle/cut_triangle.py
+++ python/demo/cut_triangle/cut_triangle.py
@@ -15,28 +15,40 @@ domain_id = cutcells.classify_cell_domain(ls_values)
 print ("domain_id =", domain_id)
 
 cell_type = cutcells.CellType.triangle
-triangulate = False
+triangulate = True
 gdim = 2
 
-cut_cell = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi<0", triangulate)
+cut_cell_int = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi<0", triangulate)
 #print(cut_cell.str())
-cut_cell.write_vtk("interior.vtu")
+cut_cell_int.write_vtk("interior.vtu")
 
-print("vertex coordinates=",cut_cell.vertex_coords)
-print("connectivity=",cut_cell.connectivity)
-print("Cell type=", cut_cell.types)
-
-cut_cell = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi>0", triangulate)
+cut_cell_ext = cutcells.cut(cell_type, vertex_coordinates,  gdim, ls_values, "phi>0", triangulate)
 #print(cut_cell.str())
-cut_cell.write_vtk("exterior.vtu")
+cut_cell_ext.write_vtk("exterior.vtu")
 
-cells = cut_cell.connectivity
-points = cut_cell.vertex_coords
-celltypes = cut_cell.types
+#add offset
+# connectivity_ext = np.zeros(cut_cell_ext.connectivity.size,dtype=np.uint32)
+# connectivity_ext[0] = cut_cell_ext.connectivity[0]
+# for i in range(1,connectivity_ext.size):
+#     connectivity_ext[i] = cut_cell_ext.connectivity[i]+cut_cell_int.connectivity[0]
 
-pv.OFF_SCREEN = True
-grid = pv.UnstructuredGrid(cells, celltypes, points)
+# cells = np.append(cut_cell_int.connectivity, connectivity_ext,axis=0)  
+# points = np.append(cut_cell_int.vertex_coords, cut_cell_ext.vertex_coords,axis=0)
+# celltypes = np.append(cut_cell_int.types,cut_cell_ext.types,axis=0)
+
+# print(cells)
+# print(points)
+# print(celltypes)
+
+#pv.OFF_SCREEN = True
+pv.start_xvfb()
+
+#grid = pv.UnstructuredGrid(cells, celltypes, points)
+
+grid_int = pv.UnstructuredGrid(cut_cell_int.connectivity, cut_cell_int.types, cut_cell_int.vertex_coords)
+grid_ext = pv.UnstructuredGrid(cut_cell_ext.connectivity, cut_cell_ext.types, cut_cell_ext.vertex_coords)
 
 plotter = pv.Plotter(off_screen=True)
-plotter.add_mesh(grid, color="orange")
+plotter.add_mesh(grid_int, color="blue",show_edges=True)
+plotter.add_mesh(grid_ext, color="red",show_edges=True)
 plotter.show(screenshot='cut_triangle.png')
